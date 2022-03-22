b5b5d9085a4c9c8c8be900da24ab00cea6318f8f
Eason Yi <lisliefor@163.com>
2022-03-22 15:52:24 +0800

    feat: 引用dumi封装的项目antd-mob-v5-ext的组件，测试样式问题

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,9 +1,14 @@
 import React, { Component } from "react";
-import '../App.css';
+import { SampleForm } from "antd-mob-v5-ext/lib";
+import "../App.css";
 
 class ProductList extends Component {
   render() {
-    return <div className="wy-products">my products.</div>;
+    return (
+      <div className="wy-products">
+        <SampleForm />
+      </div>
+    );
   }
 }
 
